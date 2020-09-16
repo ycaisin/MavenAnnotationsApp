ed1604ab80b60e6d0105e7c8a45d0d5460e2a352
ycaisin <yuliyadzyubak@gmail.com>
2020-09-16 22:51:40 +0300

    Annotations with Maven

---
+++ src/main/java/main/Box.java
@@ -0,0 +1,16 @@
+package main;
+
+import homeprovider.ca.NoStaticFields;
+import homeprovider.ca.NotStatic;
+
+@NotStatic
+public class Box {
+	public int x;
+	public static int y;
+	
+	public static void ee() {
+		
+	};
+	
+	
+}
---
+++ src/main/java/main/Cup.java
@@ -0,0 +1,14 @@
+package main;
+
+import homeprovider.ca.NoStaticFields;
+
+@NoStaticFields
+public class Cup {
+	public static int y;
+	
+	public static void ee() {
+		
+	};
+
+
+}
